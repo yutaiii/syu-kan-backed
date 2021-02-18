19e7f64961e5bbfe029a997c4481ed14afd7bc70
市川裕太 <yu74ichi69@gmail.com>
2021-02-18 22:24:29 +0900

    ユーザー管理用にデーブル定義を修正

--- tool/migrate/2_insert_test_data.up.sql
+++ tool/migrate/2_insert_test_data.up.sql
@@ -1,9 +1,16 @@
 INSERT INTO
-  routines(name, started_at)
+  users(name, fireabase_uid)
 VALUES
-  ("rotine 1", "2021-01-21 22:30:00"),
-  ("rotine 2", "2021-01-21 22:30:00"),
-  ("rotine 3", "2021-01-21 22:30:00");
+  ('test1', 'uid1'),
+  ('test2', 'uid2'),
+  ('test3', 'uid3');
+
+INSERT INTO
+  routines(name, user_id, started_at)
+VALUES
+  ("rotine 1", 1, "2021-01-21 22:30:00"),
+  ("rotine 2", 2, "2021-01-21 22:30:00"),
+  ("rotine 3", 3, "2021-01-21 22:30:00");
 
 INSERT INTO
   progress(routine_id, date)
